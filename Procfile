dce80c9a80848fead6190f69701d87bffaee4850
Jash Sharma <jashsharma619@gmail.com>
2022-02-23 00:28:18 +0530

    Heroku Settings Added

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn hirenseeks.wsgi --log-file -
--- hirenseeks/wsgi.py
+++ hirenseeks/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hirenseeks.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
