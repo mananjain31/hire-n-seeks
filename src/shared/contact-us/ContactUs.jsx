b505e88d9e8521669107939ef5ba29c6f6ba03a0
Manan Jain <mananjain31jan@gmail.com>
2022-03-04 01:44:54 +0530

    contact us in landing page : desktop view implemented

--- src/shared/contact-us/ContactUs.jsx
+++ src/shared/contact-us/ContactUs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './ContactUs.scss';
+import SocialMediaSVG from '../../assets/social-media.svg'
 
 const ContactUs = (props) => {
   return (
     <section className='contact-us page-section'>
-      <h2 className='page-section-headline'>Introducing <br/> Fresher Jobs</h2>
+
+
+      <div className="contact-us-body">
+        <h2>Contact Us</h2>
+        
+
+        <div className="contact-us-content">
+          <h3>Address</h3>
+          <p>Indore, Madhya Pradesh, India</p>
+          <h3>Email</h3>
+          <p>
+            mananjain31jan@gmail.com <br/>
+            tiwarividhya2001@gmail.com <br/>
+            jashsharma619@gmail.com <br/>
+          </p>
+          <h3>Contact Number</h3>
+          <p>+91 7000784085</p>
+        </div>
+      </div>
+
+      <div className='svg-image'>
+        <img  src={SocialMediaSVG} alt="contact us image" />
+      </div>
+
     </section>
   )
 }
