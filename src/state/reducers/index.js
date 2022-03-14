688631c53c8aed01e5bb4a974145fb1780f4663f
Manan Jain <mananjain31jan@gmail.com>
2022-03-15 01:33:31 +0530

    :white_check_mark: verified redux thunks, and backend api's working with login api

--- src/state/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import accountReducer from "./accountReducer";
-
-const reducers = combineReducers({
-  account: accountReducer,
-});
-
-export default reducers;
--- src/state/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import reducers from "./reducers";
-import thunk from "redux-thunk";
-
-export const store = createStore(
-  reducers, // combined reducers
-  {}, // this is initial state
-  applyMiddleware(thunk)
-);
