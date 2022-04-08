f44daa0a929cd8646010685f25d889f6c2d2e534
mananjain31 <mananjain31jan@gmail.com>
2022-04-09 01:18:58 +0530

    :iphone: Jobs page made responsive

--- src/pages/jobspage/JobsPage.jsx
+++ src/pages/jobspage/JobsPage.jsx
@@ -4,7 +4,7 @@ import { useLocation } from 'react-router-dom';
 import { JobCard } from '../../shared/explore-jobs/ExploreJobCard'
 import { JobCardList } from '../../shared/job-card/JobCardList';
 import Navbar from '../../shared/navbar/Navbar';
-import Filters from './Filters';
+import Filters, { FiltersForMobile } from './Filters';
 
 import './JobsPage.scss'
 
@@ -26,9 +26,14 @@ export const JobsPage = () => {
     <section className='page-section jobs-page-section'>    
         <header>
           <h1 className='text-3xl font-bold'>Jobs for freshers and remote Jobs</h1>
-        </header>  
+        </header>
+        <FiltersForMobile/>
         <main>
-          <Filters/>
+          
+          <div className="desktop-only">
+            <Filters/>
+          </div>
+
           <JobCardList jobs={[{},{},{}]}/>
         </main>
     </section>
